c008c62131bc2195611f7c7a98ecf44f0d0ad8ae
Zaiba <s.zaiba.ali@gmail.com>
2019-02-08 17:40:52 -0600

    Added xUnit Function

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -6,9 +6,28 @@ namespace UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
+        public void DivisionOFTwoNumbers()
+         {
+      var expected =  2;
+            var a= 10;
+            var b= 5;
+            var result = Division(a,b);
+            Assert.Equal(expected, result);
 
         }
+       [Theory]
+       [InlineData(10,100,10)]
+       [InlineData(1,1,1)]
+       [InlineData(18,180,10)]
+       public void DivisionOFTwo_Numbers(int res, int n1, int n2)
+            {
+            Assert.Equal(res, Division(n1, n2));
+}
+            
+        int Division(int x, int y)   
+{
+            return x / y; 
+}
+
     }
 }
