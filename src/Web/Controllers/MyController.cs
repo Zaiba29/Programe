08d246a270eb7c432a83f4438773e5aa29d5f2bc
Zaiba <s.zaiba.ali@gmail.com>
2019-02-08 17:15:26 -0600

    updated view

--- src/Web/Controllers/MyController.cs
+++ src/Web/Controllers/MyController.cs
@@ -22,10 +22,8 @@ namespace Web.Controllers
          public IActionResult MyView()
         {
             return View();
-        }
-
-        
-
+        }    
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
